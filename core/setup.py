26bd9ff42d072c0d31ac4e2b8077de60fcf87ecf
Charles M.C. Chan <rascov@gmail.com>
2015-02-02 20:26:28 +0800

    Update run-tests for core
    - Update run-tests
    - Update setup.py for Flask

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ setup(
     description = 'A Diagnosis, Analytic and Management Framework for SDN',
     author = 'D-Link NCTU Joint Research Center',
     url = 'https://github.com/dlinknctu/omniui',
-    install_requires = ['Flask', 'Flask_Cors', 'gevent', 'pymongo'],
+    install_requires = ['Flask==0.10.1', 'Flask_Cors', 'gevent', 'pymongo'],
     packages = ['src', 'src.floodlight_modules','src.trema_modules','src.pox_modules', 'test'],
     data_files = [('etc', ['etc/config.json'])],
     entry_points = {
--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-cd ui
-npm install
-npm test
+function test_ui {
+    cd ui
+    npm install
+    npm test
+    cd ..
+}
+
+function test_core_install {
+    cd core
+    sudo python setup.py install
+    cd ..
+}
+
+test_ui
+test_core_install
